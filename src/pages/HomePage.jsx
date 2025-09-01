412762e3627df1b1c2a1ade8fc5bf7156460dcb6
filip-io <filip.n@outlook.com>
2025-09-01 14:46:49 +0200

    Add link to portfolio v3

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -12,7 +12,7 @@ export const HomePage = () => {
     const handleMagicWord = (event) => {
       setTypedCharacters((prevTypedCharacters) => prevTypedCharacters + event.key.toLowerCase());
     };
-  
+
     const checkMagicWord = () => {
       if (typedCharacters.toLowerCase() === magicKeyword) {
         setShowModal(true);
@@ -40,38 +40,40 @@ export const HomePage = () => {
   };
 
   return (
-      <main className="home-main-container" style={{ backgroundColor }}>
-        <div className="home-main-title-container">
-          <div className="home-main-container-left-box">
-            <h1>Hi, I'm Filip Nilsson</h1>
-            <h2>Full-stack .NET Developer</h2>
-          </div>
-          <div className="home-main-container-right-box">
-            <img src={filipPic} alt="Picture of Filip" id="filip" className="filip-home-pic" onClick={toggleBackground} />
-          </div>
+    <main className="home-main-container" style={{ backgroundColor }}>
+      <div className="home-main-title-container">
+        <div className="home-main-container-left-box">
+          <h1>Hi, I'm Filip Nilsson</h1>
+          <h2>Full-stack .NET Developer</h2>
         </div>
-        <article className="home-intro-container">
-          <div>
-            <h3>A Few Tidbits About Me:</h3>
-          </div>
-          <div className="home-intro-box">
-            <div className="h4-container">
-              <div>
-                <i className="fa-solid fa-keyboard fa-2xl"></i>
-                <h4>Web Developer</h4>
-              </div>
-              <div>
-                <i className="fa-solid fa-face-grin-beam fa-2xl"></i>
-                <h4>UX Enthusiast</h4>
-              </div>
-              <div>
-                <i className="fa-sharp fa-solid fa-music fa-2xl"></i>
-                <h4>HiFi Nerd</h4>
-              </div>
+        <div className="home-main-container-right-box">
+          <img src={filipPic} alt="Picture of Filip" id="filip" className="filip-home-pic" onClick={toggleBackground} />
+        </div>
+        <h3>Check out the new headless version of my portfolio by <a href="https://filip-io.github.io/" target="_blank" rel="noopener noreferrer">clicking here</a></h3>
+      </div>
+
+      <article className="home-intro-container">
+        <div>
+          <h3>A Few Tidbits About Me:</h3>
+        </div>
+        <div className="home-intro-box">
+          <div className="h4-container">
+            <div>
+              <i className="fa-solid fa-keyboard fa-2xl"></i>
+              <h4>Web Developer</h4>
+            </div>
+            <div>
+              <i className="fa-solid fa-face-grin-beam fa-2xl"></i>
+              <h4>UX Enthusiast</h4>
+            </div>
+            <div>
+              <i className="fa-sharp fa-solid fa-music fa-2xl"></i>
+              <h4>HiFi Nerd</h4>
             </div>
           </div>
-        </article>
-        <MagicWordModal isOpen={showModal} onClose={closeModal} />
-      </main>
+        </div>
+      </article>
+      <MagicWordModal isOpen={showModal} onClose={closeModal} />
+    </main>
   );
 };
